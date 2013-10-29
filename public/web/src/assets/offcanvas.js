14e38bc8f878b9045d7a116180d9bdd494701cf9
wangfeiv <wangfeiv@outlook.com>
2013-10-29 22:54:13 +0800

    [Wang Fei][2013/10/29] Add a folder named "Web"
    The fold named "Web" contains the new html pages, CSS and JS files.

---
+++ public/web/src/assets/offcanvas.js
@@ -0,0 +1,11 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User:  wangfei 2012-9-20
+ * Time: 下午11:50
+ * To change this template use File | Settings | File Templates.
+ */
+$(document).ready(function() {
+    $('[data-toggle=offcanvas]').click(function() {
+        $('.row-offcanvas').toggleClass('active');
+    });
+});
